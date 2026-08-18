bea4f7e053c700de79eb7c0d23fcfa5d00fbf745
Danny <yafeng.wu@outlook.com>
2026-08-18 11:53:55 +0800

    fix: 在某些WINDOWS版本中 ALT+TAB时可能会强制切换为中文输入法

--- src/TSF_Patcher/dllmain.cpp
+++ src/TSF_Patcher/dllmain.cpp
@@ -2,38 +2,109 @@
 #include "pch.h"
 #include "pattern_finder.h"
 #include <msctf.h>
+#include <Windows.h>
+#include <iostream>
 
-typedef void(__cdecl* GameUIHookProc_t)(int*);
+typedef void(__cdecl* GameUIHookProc_t)(int*, void* wgt);
+enum class GameUICallbackEvent
+{
+	UI_Callback_GetGameCursor = 0,
+	UI_Callback_DrawCursor,
+	UI_Callback_EnableIME,
 
-static ITfDocumentMgr* s_docMgr;
+	UI_Callback_Encrypt,//密码控件改进
+	UI_Callback_Decrypt,//密码控件改进
 
-static void OnFocusChanged(int* lParam) {
-	bool hasFocus = *lParam;
-	ITfThreadMgr* pThreadMgr = nullptr;
-	TfClientId clientId;
-	// 创建 ITfThreadMgr 对象
-	if (SUCCEEDED(CoCreateInstance(CLSID_TF_ThreadMgr, nullptr, CLSCTX_INPROC_SERVER, IID_ITfThreadMgr, (void**)&pThreadMgr))) {
-		// 激活 TSF
-		if (SUCCEEDED(pThreadMgr->Activate(&clientId))) {
-			
-			if (s_docMgr == nullptr) {
-				// 自定义文档，什么都没设置，所以不能输中文
-				pThreadMgr->CreateDocumentMgr(&s_docMgr);
-			}
+	UI_Callback_IsKeyChar,
+	UI_Callback_GetKeyChar,
+	UI_Callback_DrawKeyChar,
+	UI_Callback_HitKeyChar,
+	UI_Callback_ValidKeyChar,
 
-			if (hasFocus) {
-				// 设置为默认文档
-				pThreadMgr->SetFocus(NULL);
-			}
-			else {
-				// 设置为自定义文档
-				pThreadMgr->SetFocus(s_docMgr);
-			}
+	UI_Callback_IsValidItem,
 
-			pThreadMgr->Deactivate();
+	UI_Callback_QueryItemInfo,
+	UI_Callback_UpdateItemInfo,
+
+	UI_Callback_ClickItem,
+	UI_Callback_DClickItem,
+	UI_Callback_RClickItem,
+	UI_Callback_MoveItem,
+	UI_Callback_DrawItem,
+
+	UI_Callback_BeginDragItem,
+	UI_Callback_EndDragItem,
+
+	UI_Callback_GetItemPixel,
+
+	UI_Callback_QueryDirty,
+
+	UI_Callback_DrawCustomBuffer,
+	UI_Callback_SetCustomBuffer,
+	UI_Callback_GetCustomBuffer,
+	UI_Callback_ParseCustomBuffer,
+	UI_Callback_CustomBufferSelfShow,
+	UI_Callback_CustomBufferDeinit,
+
+	UI_Callback_QueryNode,
+	UI_Callback_QueryNodePath,
+
+	UI_Callback_PlaySound,
+
+	UI_Callback_RequestSoftInput,
+
+	//事件最大数目
+	UI_Callback_MAXNUM,
+
+};
+inline static void ThreadMgr_EnableIME(ITfThreadMgr* pThreadMgr, bool bEnable) {
+	// in-game ime: forbid input method editor (IME) to input text.
+	static ITfDocumentMgr* s_DocMgr = nullptr;
+	// user ime: for input text
+	static ITfDocumentMgr* s_PrevDocMgr = nullptr;
+
+	auto hWnd = GetActiveWindow();// Game Window
+	// init in-game ime at first time.
+	if (s_DocMgr == nullptr) {
+		if (FAILED(pThreadMgr->CreateDocumentMgr(&s_DocMgr))) {
+			return;
 		}
-		pThreadMgr->Release();
+
+		// set in-game ime as default document manager. user cannot input any text.
+		pThreadMgr->AssociateFocus(hWnd, s_DocMgr, &s_PrevDocMgr);
 	}
+
+	ITfDocumentMgr* pTempDocMgr = NULL;
+	if (bEnable) {
+		// restore user ime.
+		pThreadMgr->AssociateFocus(hWnd, s_PrevDocMgr, &pTempDocMgr);
+		//pThreadMgr->SetFocus(s_PrevDocMgr);
+	}
+	else {
+		// in-game ime
+		pThreadMgr->AssociateFocus(hWnd, s_DocMgr, &pTempDocMgr);
+		//pThreadMgr->SetFocus(s_DocMgr);
+	}
+
+	pTempDocMgr->Release();
+}
+
+static void OnFocusChanged(int* data, void* wgt) {
+	auto bIme = *reinterpret_cast<bool*>(data);
+	ITfThreadMgr* pThreadMgr = nullptr;
+	if (FAILED(CoCreateInstance(CLSID_TF_ThreadMgr, nullptr, CLSCTX_INPROC_SERVER, IID_ITfThreadMgr, (void**)&pThreadMgr))) {
+		return;
+	}
+
+	TfClientId clientId = TF_CLIENTID_NULL;
+
+	if (SUCCEEDED(pThreadMgr->Activate(&clientId))) {
+		ThreadMgr_EnableIME(pThreadMgr, bIme);
+
+		pThreadMgr->Deactivate();
+	}
+
+	pThreadMgr->Release();
 }
 
 
@@ -43,22 +114,22 @@ static void PatchAsync(LPARAM lParam) {
 
 	auto hModule = GetModuleHandleA(NULL);
 
-	auto pGameUIHooksBase = (char*)pattern::search("8B 44 24 04 8B 04 85 ?? ?? ?? ?? 85 C0 74 0B 51", hModule) + 7;
+	auto pVecCallbackFuncsBase = (char*)pattern::search("8B 44 24 04 8B 04 85 ?? ?? ?? ?? 85 C0 74 0B 51", hModule) + 7;
 
-	if (pGameUIHooksBase == 0) {
-		MessageBoxA(NULL, "Failed to find GameUIHooksBase", "Error", MB_OK);
+	if (pVecCallbackFuncsBase == 0) {
+		MessageBoxA(NULL, "Failed to find g_pVecCallbackFuncs", "Error", MB_OK);
 
 		return;
 	}
 
-	auto pGameUIHooks = *(GameUIHookProc_t**)pGameUIHooksBase;
+	auto g_pVecCallbackFuncs = *(GameUIHookProc_t**)pVecCallbackFuncsBase;
 	// wait game initialize global variable: 'pGameUIHooks'
-	while (pGameUIHooks == 0 || pGameUIHooks[31] == nullptr) {
+	while (g_pVecCallbackFuncs == 0 || g_pVecCallbackFuncs[31] == nullptr) {
 		Sleep(100);
-		pGameUIHooks = *(GameUIHookProc_t**)pGameUIHooksBase;
+		g_pVecCallbackFuncs = *(GameUIHookProc_t**)pVecCallbackFuncsBase;
 	}
 
-	pGameUIHooks[31] = OnFocusChanged;
+	g_pVecCallbackFuncs[static_cast<int>(GameUICallbackEvent::UI_Callback_RequestSoftInput)] = OnFocusChanged;
 
 	printf("TSF patch success\n");
 }
